91eee3253775529f9eb9eb66b77b7131800fb853
Sanjeev Jha <sjha2@sastasundar.com>
2024-05-16 16:43:20 +0530

    variables added

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "Sanjeev Jha";
+$name1 = "Eklavya Rathi";
+
+echo "Hello {$name} <br>";
+$name = 1;
+echo "Hello {$name} <br>";
+echo "Hello {$name1} <br>";
+
+$item = 2.5;
+$price = 100.23;
+echo "Total price is \$" . ($item * $price);
+
+?>
